026505a6d6d86d666654660689547fae9a18d4d1
Vignevig <vignesh93443@gmail.com>
2026-04-07 09:48:08 +0530

    Adding express

--- server.js
+++ server.js
@@ -2,15 +2,15 @@ const http = require('http');
 
 const fs = require('fs')
 
-
-
-
 const server = http.createServer((req,res)=>{
     //res.setHeader('content-type','text/html',"I am the header dad deii");
     const url=req.url;
+    const method=req.method;
 
-       if(url=='/message') {
+       if(url=='/message' && method=='POST') {
         res.setHeader('Location','/');
+
+        const body = []
 
         req.on('data',(chunk)=>{
 
@@ -18,26 +18,20 @@ const server = http.createServer((req,res)=>{
 
         })
         fs.writeFileSync('welcome.txt','this is my first fs module expirement\n');
-
         res.statusCode = 302;
         res.write('<html>');
         res.write('<head><title>NodeJs</title></head>');
         res.write('<body><h2>Welcome to the node js. This is my first node.js server</h2></body>');
         res.write('</html>');
-
-        return res.end();
-
+        res.end('Receiving data');
        }
-        
-
-
     if(url=='/'){
         res.write('<html>')
         res.write('<body><form id="form" action="/message" method ="POST"><input type="text" name="username"><input  type="submit" value="send"></form></body>')
         res.write('</html>')
     }
-   
     res.end();
+
 });
 
 server.listen(3000,() => {
